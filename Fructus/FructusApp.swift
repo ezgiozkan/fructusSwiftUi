a793a91c5cda03ef845fb1eb6d0592d96aecb9ea
ezgi <ezgi.ozkan@redevio.com>
2021-03-20 23:10:10 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by ezgi on 20.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
